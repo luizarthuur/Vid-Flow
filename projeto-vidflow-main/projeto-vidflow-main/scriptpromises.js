f65334d49e3ac56d4ae55342ab94b13c17230210
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-05-01 21:05:29 -0300

    Update

--- projeto-vidflow-main/projeto-vidflow-main/scriptpromises.js
+++ projeto-vidflow-main/projeto-vidflow-main/scriptpromises.js
@@ -1,5 +1,25 @@
+const containerVideos = document.querySelector('.videos__container');
+
 const api = fetch('http://localhost:3000/videos') //Promise de captura de dados da API - pode retornar positivo (fullfilled) ou negativo (rejected) ou estar pendente (pending)
-.then(res => console.log(res.json())) //Tratamento da promise usando then (Código executará o then só após o retorno da promise (negativo ou positivo) assim transformando essa resposta em um JSON)
+api.then(res => (res.json())) //Tratamento da promise usando then (Código executará o then só após o retorno da promise (negativo ou positivo) assim transformando essa resposta em um JSON)
+api.then((videos) => {
+    videos.forEach(video => {
+        containerVideos.innerHTML +=`
+        <li class="videos__item">
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>
+            </div>
+        </li>
+        `;
+    });
+})
+api.catch((error) => {
+    containerVideos.innerHTML = `<p>Ocorreu um erro ao carregar os vídeos:</p>` + error
+})
+
 
 //As promises podem levar dois argumentos resolve (solução) e reject (erro)
 
